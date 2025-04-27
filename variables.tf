74b9b9fcf7060c484f298d23defec6eb7ada735c
YogaMing1 <59271968+YogaMing1@users.noreply.github.com>
2025-04-27 21:04:06 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -115,6 +115,21 @@ variable "buckets" {
   }))
 }
 
+variable "docker_ports" {
+  type = list(object({
+    internal = number
+    external = number
+    protocol = string
+  }))
+  default = [
+    {
+      internal = 8300
+      external = 8300
+      protocol = "tcp"
+    }
+  ]
+}
+
 
 
 
