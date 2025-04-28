ad8cb0fcb4a3cf8a9b2c9a0c4d6232db51736793
YogaMing1 <59271968+YogaMing1@users.noreply.github.com>
2025-04-28 10:36:17 +0800

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,49 @@
+output "api_base_url" {
+  value = "https://${aws_instance.example.private_dns}:8433/"
+
+  # The EC2 instance must have an encrypted root volume.
+  precondition {
+    condition     = data.aws_ebs_volume.example.encrypted
+    error_message = "The server's root volume is not encrypted."
+  }
+}
+
+output "secret_id" {
+  value       = aws_secretsmanager_secret.secret_id
+  description = "Temporary secret ID for accessing database in AWS."
+  ephemeral   = true
+}
+
+output "db_password" {
+  value       = aws_db_instance.db.password
+  description = "The password for logging in to the database."
+  sensitive   = true
+}
+
+output "out" {
+  value     = "xyz"
+  sensitive = true
+}
+
+output "a" {
+  value     = "secret"
+  sensitive = true
+}
+
+output "instance_ip_addr" {
+  value       = aws_instance.server.private_ip
+  description = "The private IP address of the main server instance."
+
+  depends_on = [
+    # Security group rule must be created before this IP address could
+    # actually be used, otherwise the services will be unreachable.
+    aws_security_group_rule.local_access,
+  ]
+}
+
+
+
+
 locals {
   redshift_route_table_ids = aws_route_table.redshift[*].id
   public_route_table_ids   = aws_route_table.public[*].id
